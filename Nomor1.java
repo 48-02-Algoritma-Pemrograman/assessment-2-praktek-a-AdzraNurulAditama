e0f19c9cca2f72c8560517ee6b4623516da477d7
AdzraNurulAditama <adzraaditama06@gmail.com>
2024-12-12 12:42:14 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
 
-public class Nomor1 {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+public class Nomor1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukan nilai N: ");
+        int N = scanner.nextInt();
+        menampilkanDeret(N);
+        scanner.close();
     }
-    
+
+    public static void menampilkanDeret(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i == 0){
+                System.out.println(i);
+            }
+            else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
